b78f1d7520c3641abb78048929d1bd7547df9dcb
derricknjeru <derricknjeru5@gmail.com>
2022-03-25 14:57:25 +0300

    initial commit

---
+++ guessing_game/target/rls/debug/deps/guessing_game-1669e9edb30acf46.d
@@ -0,0 +1,5 @@
+/home/derricknjeru/projects/learning/rust_projects/guessing_game/target/rls/debug/deps/guessing_game-1669e9edb30acf46.rmeta: src/main.rs
+
+/home/derricknjeru/projects/learning/rust_projects/guessing_game/target/rls/debug/deps/guessing_game-1669e9edb30acf46.d: src/main.rs
+
+src/main.rs:
